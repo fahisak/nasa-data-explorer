bd33148ed56f9d64048af22ac574531cb1333bd9
fahisak19997 <93871075+Fahisakollath@users.noreply.github.com>
2024-09-30 14:15:48 +0000

    Update CORS configuration to allow requests from localhost and Netlify

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const nasaRoutes = require('./src/routes/nasaroutes'); 
-const logger=require('./src/helper/logger')
+const logger = require('./src/helper/logger');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+// Configure CORS to allow requests from localhost and your Netlify site
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://nasa-data-explorer.netlify.app'],  // Allow both local and Netlify
+}));
 
+app.use(express.json());
 
 app.use('/api/nasa', nasaRoutes);
 
+// Start the server
 app.listen(PORT, () => {
-  logger.info(`Server is running on port :${PORT}`)
+  logger.info(`Server is running on port: ${PORT}`);
 });
